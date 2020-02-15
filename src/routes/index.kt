e228e28ba8057cf4df3d40061d6756a58b3e2006
ari dwi prayogo <kisamberbledek@yahoo.com>
2020-02-15 07:33:59 +0700

    cleanup code

--- src/routes/index.kt
+++ src/routes/index.kt
@@ -1,12 +1,12 @@
 external fun require(module: String): dynamic
 
 val Router = require("express").Router
-const val message: String = "halo semua";
+const val MESSAGE: String = "halo semua";
 fun create() {
   val r = Router();
     
   r.get("/", { _, res ->
-    res.send(message)
+    res.send(MESSAGE)
   })
 
   return r 
